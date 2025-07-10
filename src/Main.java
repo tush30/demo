34fa92e31b9bc7c397c931e0eb30af33c2c4eda4
tush30 <105384785+tush30@users.noreply.github.com>
2025-07-10 16:15:30 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,22 @@ import java.util.Arrays;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            String a ="ZASKFDLklhfsdfsDLJFSJGIHEKHIPEINNNFIGHKkjgksfgjrotyotoyjtkjkLJOIOEHEKHKKDJGKFGJkfjhglfhjtrhkjfkhjnfglhkjflgjhtrljhfljhfgljhfgljhfgljhtrklyjhtrkjhfgkljhfgjhfljhtrljlfjhfgljhfglkjhflyjtljtrlyjhtryjtrtykhrktherktjhtrkyjhkujhtykhtryhrthHKLJHLHRLHTLRHLKHTRLKHLHRLHLKHLKHKLHLKHLHKLHKHJKHKJHKJHJKHKHJKHKHHLHLHLHKHKJHKJKKHKHKHKHKHHKHKHKHKHkhktryhtlhtklhtrkyhtrkyhtrkjyhtrkyhrekthtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkrtkyhtrklyhjrOEOHKDHFksdhfklHLHKHLHKKJHJHKGKLHLHJLJHLHLHLHLHHLHLHLHH";
-            int j=1;
-            // to aviod index
-            int k=j-1;
-            String [] arr =new String [a.length()-k];
-            for(int i=0;i<=a.length()-1;i++) {
-                if(j<=a.length()) {
-                    System.out.println(a.substring(i, j));
-                    arr[i]=a.substring(i, j);
-                    j++;
-                }
-            }
-            Arrays.sort(arr);
-            System.out.println(arr[0]+" and largest number is "+arr[arr.length-1]);
-          System.out.println(Arrays.toString(arr));
+          //   String a ="ZASKFDLklhfsdfsDLJFSJGIHEKHIPEINNNFIGHKkjgksfgjrotyotoyjtkjkLJOIOEHEKHKKDJGKFGJkfjhglfhjtrhkjfkhjnfglhkjflgjhtrljhfljhfgljhfgljhfgljhtrklyjhtrkjhfgkljhfgjhfljhtrljlfjhfgljhfglkjhflyjtljtrlyjhtryjtrtykhrktherktjhtrkyjhkujhtykhtryhrthHKLJHLHRLHTLRHLKHTRLKHLHRLHLKHLKHKLHLKHLHKLHKHJKHKJHKJHJKHKHJKHKHHLHLHLHKHKJHKJKKHKHKHKHKHHKHKHKHKHkhktryhtlhtklhtrkyhtrkyhtrkjyhtrkyhrekthtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkhtrkyhtrkrtkyhtrklyhjrOEOHKDHFksdhfklHLHKHLHKKJHJHKGKLHLHJLJHLHLHLHLHHLHLHLHH";
+          //   int j=1;
+          //   // to aviod index
+          //   int k=j-1;
+          //   String [] arr =new String [a.length()-k];
+          //   for(int i=0;i<=a.length()-1;i++) {
+          //       if(j<=a.length()) {
+          //           System.out.println(a.substring(i, j));
+          //           arr[i]=a.substring(i, j);
+          //           j++;
+          //       }
+          //   }
+          //   Arrays.sort(arr);
+          //   System.out.println(arr[0]+" and largest number is "+arr[arr.length-1]);
+          // System.out.println(Arrays.toString(arr));
+        System.out.println("hello word");
 
 ;
     }
